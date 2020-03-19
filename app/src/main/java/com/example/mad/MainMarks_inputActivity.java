59b2000cf75f1ce4a9221c720379bf7b83df0cac
JANA <janakamadushankadc@gmail.com>
2020-03-19 10:16:21 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mad/MainMarks_inputActivity.java
@@ -0,0 +1,14 @@
+package com.example.mad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainMarks_inputActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_marks_input);
+    }
+}
