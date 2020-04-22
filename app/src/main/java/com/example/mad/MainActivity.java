3f16945afcd81661710b4d76cd412b09d03aa769
JANA <janakamadushankadc@gmail.com>
2020-04-22 12:13:36 +0530

    part of Kethmini and himanshi.

---
+++ app/src/main/java/com/example/mad/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.mad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main2);
+        //setContentView(R.layout.activity_main_login);
+        //setContentView(R.layout.activity_main_signup);
+        //setContentView(R.layout.activity_main_marks_vw);
+        //setContentView(R.layout.activity_main_marks_input);
+        //setContentView(R.layout.activity_main_alltable);
+        setContentView(R.layout.activity_himshi1);
+        //setContentView(R.layout.activity_himshi2);
+    }
+}
---
+++ app/src/main/java/com/example/mad/himshi1.java
@@ -0,0 +1,14 @@
+package com.example.mad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class himshi1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_himshi1);
+    }
+}
